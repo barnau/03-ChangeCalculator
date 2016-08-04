a535917f66af0c1787d8ad9649c577f194e92368
Blaine Arnau <blainearnau@gmail.com>
2016-08-04 15:13:05 -0700

    Change app built with bootstrap and jquery with input validation

---
+++ javascript/app.js
@@ -0,0 +1,43 @@
+$(".btn").click(function() {
+	$("#errorPanel").hide();
+	
+
+    var saleAmount = $("#saleAmount").val();
+    var amountReceived = $("#amountReceived").val();
+
+
+
+
+    if (saleAmount <= 0 || isNaN(saleAmount) || amountReceived <= 0 || isNaN(saleAmount) || (amountReceived < saleAmount)) {
+
+    	$(".panel-body").text("Please enter positive values and be sure amount received is greater or equal to the sale price.");
+    	$("#errorPanel").show();
+    } else {
+
+
+        var change = amountReceived - saleAmount;
+        var dollars = Math.floor(change);
+        var coins = Math.round((change - dollars) * 100);
+        var quarters = Math.floor(coins / 25);
+        var coinsAfterQuarters = coins % 25;
+        var dimes = Math.floor(coinsAfterQuarters / 10);
+        var coinsAfterDimes = coinsAfterQuarters % 10;
+        var nickels = Math.floor(coinsAfterDimes / 5);
+        var pennies = coinsAfterDimes % 5;
+
+
+
+        $("#dollarsPanel").text(dollars);
+        $("#quartersPanel").text(quarters);
+        $("#dimesPanel").text(dimes);
+        $("#nickelsPanel").text(nickels);
+        $("#penniesPanel").text(pennies);
+
+    }
+
+
+
+
+
+
+})
